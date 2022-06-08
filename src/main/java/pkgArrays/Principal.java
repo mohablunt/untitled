8d4177ee34cbcf660fec8d1376e973fdb3d360da
mohablunt <mohablunt@gmail.com>
2022-06-08 17:04:28 +0200

    Initial commit

---
+++ src/main/java/pkgArrays/Principal.java
@@ -0,0 +1,15 @@
+package pkgArrays;
+
+import pkgArray.Vectors;
+import pkgArray.Matrius;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Vectors v = new Vectors();
+        int [] vector=v.crearVector(5);
+        int a = 3;
+
+    }
+
+}
---
+++ src/main/java/pkgArrays/provamatru.java
@@ -0,0 +1,14 @@
+package pkgArrays;
+
+import pkgArray.Matrius;
+
+public class provamatru {
+
+        public static void main(String[] args) {
+
+            Matrius mat = new Matrius();
+            int maxF=10,maxC=10;
+            int[][] m=mat.creaMatriu(maxF,maxC);
+        }
+
+}
